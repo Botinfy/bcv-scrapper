ccc187495fbc10c965ea8eeb5f976f3d568db237
jwsequera <jw.sequera@gmail.com>
2025-01-30 13:26:12 -0400

    fix: prisma

---
+++ prisma/migrations/20250129222810_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Rate" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "date" DATETIME NOT NULL,
+    "rate" REAL NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
